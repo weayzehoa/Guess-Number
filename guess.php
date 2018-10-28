bb5afa9a1ac3e196b5231d7263b0da1dde414ebd
Roger Wu <weayzehoa@gmail.com>
2018-10-28 14:57:11 +0800

    1.01
    Bug fix (修正檢驗字元)

--- guess.php
+++ guess.php
@@ -5,13 +5,13 @@
 
 <?php
 session_start();
-$guess["num"]= Array();
+$guess["num"]= Array(); //宣告 $guess["num"] 為陣列
 
-if (empty($_POST["guess"]) || (int)$_POST["guess"]/1000 <1 ) { //如果未輸入任何字 或 輸入其他字元 或者 整數除以1000小於1
-echo "請輸入四個數字<br>";
+if (empty($_POST["guess"]) || !is_numeric($_POST["guess"])) { //如果未輸入任何字 或 輸入其他字元, is_numeric用來判斷是否為數字, !為相反
+echo "請輸入四個數字, 請勿空白或輸入其他字元<br>";               //顯示警告訊息
 }
 else{
-    $guess["num"] = preg_split('//', $_POST["guess"], -1, 1); //將得到的字串拆成單個並放入陣列中
+    $guess["num"] = preg_split('//', $_POST["guess"], -1, 1); //將得到的數字字串拆成單個並放入$guess["num"]陣列中
     $c=0;
     if ($guess["num"][0]==$guess["num"][1]) {$c=$c+1; }       //如果陣列內數字比對相同則 $c+1
     if ($guess["num"][0]==$guess["num"][2]) {$c=$c+1; }       //如果陣列內數字比對相同則 $c+1
@@ -19,9 +19,10 @@ else{
     if ($guess["num"][1]==$guess["num"][2]) {$c=$c+1; }       //如果陣列內數字比對相同則 $c+1
     if ($guess["num"][1]==$guess["num"][3]) {$c=$c+1; }       //如果陣列內數字比對相同則 $c+1
     if ($guess["num"][2]==$guess["num"][3]) {$c=$c+1; }       //如果陣列內數字比對相同則 $c+1
-    if ($c>=1) {
-        echo "請勿輸入重複數字";
+    if ($c>=1) {                                              //如果c>=1代表有數字相同
+        echo "請勿輸入重複數字<br>";                           //顯示警告訊息
     }
+
     else {
         $a=0;$b=0;                          //宣告 $a, $b變數從0開始
         for ($i=0; $i < 4; $i++) {              //比對兩個陣列的迴圈
@@ -32,11 +33,11 @@ else{
                 }
             }
         }
-    
-        $_SESSION["times"] = $_SESSION["times"]+1;
-        $times=$_SESSION["times"];
-        $_SESSION["rec"][($times - 1)]="第 ".$_SESSION["times"]." 次 ".$_POST["guess"]." == ".$a."A ".$b."B<br>";
-       
+        
+            $_SESSION["times"] = $_SESSION["times"]+1;              //利用session陣列來存放猜的次數
+            //$times=$_SESSION["times"];                              //轉換變數 以利操作
+            $_SESSION["rec"][($_SESSION["times"] - 1)]="第 ".$_SESSION["times"]." 次 ".$_POST["guess"]." == ".$a."A ".$b."B<br>"; //儲存記錄陣列
+        
         if ($a==4) {        //如果答案4A則顯示全部猜對, 並清除session 或 跳出別的頁面.
             if ($_SESSION["times"]==1) {
             echo "你太厲害了, 猜 ".$_SESSION["times"]." 次就中";
@@ -51,6 +52,7 @@ else{
         
     }
 }
+
 ?>
 
 
